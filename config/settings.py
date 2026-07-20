195614c2a12cba0a36b3fe68531065dae3efbf89
Nikolay Panov <panovn.s@yandex.ru>
2026-07-20 23:11:17 +0900

    setting finish

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fallback-key-for-dev')
 DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
