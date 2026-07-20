54645e37d79dcac878fbd278b0f9ba5448c56c76
Nikolay Panov <panovn.s@yandex.ru>
2026-07-21 00:04:25 +0900

    add urls correct

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Добро пожаловать в трекер привычек! <a href='/api/habits/'>/api/habits/</a>")
 
 urlpatterns = [
+    path('', home),
     path("admin/", admin.site.urls),
     path("api/", include("habits.urls")),
     # JWT
