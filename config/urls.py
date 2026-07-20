b25145414221bbff24aaa72ba06bbd0afcd53cbb
Nikolay Panov <panovn.s@yandex.ru>
2026-07-21 00:20:11 +0900

    urls

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from django.http import HttpResponse
 
 
 def home(request):
@@ -11,16 +11,10 @@ def home(request):
 
 urlpatterns = [
     path('', home, name='home'),
-    path("admin/", admin.site.urls),
-    path("api/", include("habits.urls")),
-    # JWT
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    # Документация
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path(
-        "api/schema/swagger-ui/",
-        SpectacularSwaggerView.as_view(url_name="schema"),
-        name="swagger-ui",
-    ),
+    path('admin/', admin.site.urls),
+    path('api/', include('habits.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
