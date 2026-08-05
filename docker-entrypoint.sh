4785496a5e1104f7bd1860a3e04e157fc98c9623
Nikolay Panov <panovn.s@yandex.ru>
2026-08-06 00:13:20 +0900

    Update docker-entrypoint for prod

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 set -e
 
-# Ожидаем, пока PostgreSQL запустится
-echo "Waiting for PostgreSQL to start..."
-while ! nc -z $DB_HOST $DB_PORT; do
-  sleep 0.5
-done
-echo "PostgreSQL started."
-
-# Выполняем миграции
-python manage.py migrate
-
-# Собираем статику
-python manage.py collectstatic --noinput
-
+# Никаких проверок и миграций – всё делается вручную
 exec "$@"
